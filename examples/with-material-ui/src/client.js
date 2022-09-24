ea81be51f9c3d54209555f9c73d3807c6c873737
Tom McLaughlin <tom@codedown.io>
2022-09-24 17:53:57 -0600

    Minimize one more thing

--- examples/with-material-ui/src/client.js
+++ examples/with-material-ui/src/client.js
@@ -1,17 +1,12 @@
 
-import App from './App';
-import React from 'react';
-import { hydrate } from 'react-dom';
+import App from "./App";
+import React from "react";
+import { hydrate } from "react-dom";
 
 hydrate(
   <App />,
-  document.getElementById('root'),
+  document.getElementById("root"),
   () => {
-    // [ReHydratation](https://github.com/cssinjs/jss/blob/master/docs/ssr.md)
-    const jssStyles = document.getElementById('jss-ssr');
-    if (jssStyles && jssStyles.parentNode)
-      jssStyles.parentNode.removeChild(jssStyles);
-
     console.log("Done hydrating");
   }
 );
