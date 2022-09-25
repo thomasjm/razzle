c784a0026722a884963d60b1fbffd361b35345ef
Tom McLaughlin <tom@codedown.io>
2022-09-25 03:45:40 -0600

    Repro of loadable-components CSS issue

--- examples/with-loadable-components/src/App.js
+++ examples/with-loadable-components/src/App.js
@@ -1,6 +1,8 @@
-import "./App.css";
 import React from "react";
+import "./App.css";
 import loadable from "@loadable/component";
+
+import "tachyons";
 
 const Header = loadable(() => import("./Header"));
 const Body = loadable(() => import("./Body"));
@@ -8,7 +10,7 @@ const Footer = loadable(() => import("./Footer"));
 
 const App = () => (
   <div>
-    <h3>Welcome to the Razzle</h3>
+    <h3 className="bg-red">Welcome to the Razzle</h3>
     <Header />
     <Body />
     <Footer />
